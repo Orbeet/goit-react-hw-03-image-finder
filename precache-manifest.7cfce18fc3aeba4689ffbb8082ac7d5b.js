450dee73a97dd9fae448512e7d43f51eecca133c
orbeet <excenter8@gmail.com>
2020-08-17 18:47:05 +0300

    Updates

---
+++ precache-manifest.7cfce18fc3aeba4689ffbb8082ac7d5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba0cf608f75c1a9792bf3c470b25c07",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "0604600794124605e312",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f8c254e5.chunk.css"
+  },
+  {
+    "revision": "abe0036636fb7712c9bb",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.ad612abb.chunk.css"
+  },
+  {
+    "revision": "0604600794124605e312",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.04946f02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.04946f02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abe0036636fb7712c9bb",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.4d8131fb.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
